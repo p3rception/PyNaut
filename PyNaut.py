84f679fca9880fe3f2b7498a1fbe9e0cc9cd7732
dimitris-per <dim.pergel@gmail.com>
2023-09-15 01:13:45 +0300

    Added colors and msg prefixes

--- PyNaut.py
+++ PyNaut.py
@@ -6,7 +6,26 @@ import nmap
 import re
 import argparse
 from time import sleep
+import shutil
 
+# ---------------- Colors ---------------- #
+
+MAIN = '\033[38;5;50m'
+PLOAD = '\033[38;5;119m'
+GREEN = '\033[38;5;47m'
+BLUE = '\033[0;38;5;12m'
+ORANGE = '\033[0;38;5;214m'
+RED = '\033[1;31m'
+END = '\033[0m'
+BOLD = '\033[1m'
+
+# ----------- Message Prefixes ----------- #
+
+INPUT = f'{MAIN}Input{END}'
+WARN = f'{ORANGE}Warning{END}'
+INFO = f'{ORANGE}Info{END}'
+ERROR = f'{RED}Error{END}'
+DEBUG = f'{ORANGE}Debug{END}'
 
 # -------------- Arguments -------------- #
 
@@ -16,7 +35,7 @@ parser.add_argument("port_range", nargs="?", help="Port range to scan in format 
 
 args = parser.parse_args()
 
-# -------------- Banner -------------- #
+# ---------------- Banner ---------------- #
 
 def haxor_print(text, leading_spaces=0):
     text_chars = list(text)
@@ -27,13 +46,14 @@ def haxor_print(text, leading_spaces=0):
         current += original
         mutated += f'\033[1;38;5;82m{text_chars[i].upper()}\033[0m'
         print(f'\r{" " * leading_spaces}{mutated}', end='')
-        sleep(0.05)
+        sleep(0.07)
         print(f'\r{" " * leading_spaces}{current}', end='')
         mutated = current
 
     print(f'\r{" " * leading_spaces}{text}\n')
 
-def print_banner():
+def print_banner(): 
+
     print('\r')
     padding = '  '
     end = '\001\033[0m\002'
@@ -71,6 +91,11 @@ def print_banner():
 
     print(f"   {''.join(final)}{end}")
     haxor_print('by p3rception', 17)
+    
+    # Dynamic horizontal line
+    terminal_width = shutil.get_terminal_size().columns
+    dynamic_line = '─' * terminal_width
+    print(f"{dynamic_line}\n")
 
 
 
@@ -100,11 +125,13 @@ def scan_ports(target_ip, port_min, port_max):
         try:
             result = nm.scan(target_ip, str(port))
             port_status = result['scan'][target_ip]['tcp'][port]['state']
-            print(f"Port {port} is {port_status}")
             if port_status == 'open':
+                print(f"[{GREEN}✔{END}] Port {port} is {port_status}")
                 open_ports.append(port)
+            else:
+                print(f"[{RED}✘{END}] Port {port} is {port_status}")
         except nmap.PortScannerError as e:
-            print(f"Error scanning port {port}: {e}")
+            print(f"[{ERROR}] Couldn't scan port {port}: {e}")
 
     return open_ports
 
@@ -113,11 +140,11 @@ def main():
     
     # Prompts the user for target_ip if the argument is empty.
     while args.target_ip is None or not validate_ip(args.target_ip):
-        args.target_ip = input("Please enter a valid IP address that you want to scan: ")
+        args.target_ip = input(f"[{INPUT}] Please enter a valid {BOLD}IP address{END} that you want to scan: ")
 
     # Prompts the user for port_range if the argument is empty.
     while args.port_range is None:
-        port_range_input = input("Please enter the port range to scan in format <int>-<int> (e.g. 80-100): ")
+        port_range_input = input(f"\n[{INPUT}] Please enter the {BOLD}port range{END} to scan in format <int>-<int> (e.g. 80-100): ")
         port_min, port_max = validate_port_range(port_range_input)
         if port_min is not None and port_max is not None:
             args.port_range = f"{port_min}-{port_max}"
@@ -131,9 +158,9 @@ def main():
 
     # Print scan summary.
     if open_ports:
-        print(f"Open ports on {args.target_ip}: {', '.join(map(str, open_ports))}")
+        print(f"[{INFO}] Open ports on {args.target_ip}: {', '.join(map(str, open_ports))}")
     else:
-        print(f"No open ports found on {args.target_ip}.")
+        print(f"[{INFO}] No open ports found on {args.target_ip}.")
 
 if __name__ == "__main__":
     main()
